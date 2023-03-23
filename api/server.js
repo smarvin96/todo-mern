8d72377812736d29ec53c1bfa86eba08d253fc43
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-03-23 14:44:03 +0100

    server/todo fix

--- api/server.js
+++ api/server.js
@@ -15,4 +15,12 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
+const Todo = require("./models/Todo.js");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
 app.listen(3001, () => console.log("Server started on port 3001"));
