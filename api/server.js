0d27d463db4936fae723068dcded7af2ac1e621f
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-03-23 15:13:42 +0100

    server/request done

--- api/server.js
+++ api/server.js
@@ -33,4 +33,21 @@ app.post("/todo/new", (req, res) => {
   res.json(todo);
 });
 
+
+app.delete("/todo/delete/:id", async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json(result);
+})
+
+app.put("/todo/complete/:id", async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+
+    todo.save();
+
+    res.json(todo);
+})
+
 app.listen(3001, () => console.log("Server started on port 3001"));
