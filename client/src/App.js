054456af08faf39ff7a95d65d64aa14732ad2aea
Marvin Sprenger <sprenger-marvin@outlook.de>
2023-03-24 10:23:21 +0100

    funktioniert jetzt auch ohne refresh

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   };
 
   const completeTodo = async id => {
-    const data = fetch(API_BASE + "/todo/complete/" + id)
+    const data = await fetch(API_BASE + "/todo/complete/" + id)
       .then((res) => res.json());
 
     setTodos(todos => todos.map(todo => {
